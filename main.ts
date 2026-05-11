4a7f3c1fd0f9600b7a087c3fafaea3645a759bf9
Yuri <yuri@theyurig.com>
2026-05-11 15:31:08 -0300

    feat: expand main.ts testing

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
+import { Host } from "./interfaces/host/Host.ts";
 import { getHosts, getMacros } from "./mod/mod.ts";
 
-const hosts = await getHosts({
+const hosts: Array<Host> = await getHosts({
   // countOutput: true,
   selectInterfaces: ["ip", "dns", "hostid", "available"],
   output: ["status", "name"],
   selectParentTemplates: ["templateid", "name"],
+  selectInventory: ["location", "location_lat", "location_lon", "macaddress_a"],
 });
 console.log(hosts);
 
